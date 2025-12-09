67d252dacc23c086231faefba01a10d83a8e93af
Amanda <amanda@tsubasamanda.sh>
2025-12-09 12:25:33 -0500

    Add about window

---
+++ src/ui/about.rs
@@ -0,0 +1,45 @@
+pub struct AboutWindow {
+    alive: bool
+}
+
+impl Default for AboutWindow {
+    fn default() -> Self {
+        Self { alive: true }
+    }
+}
+
+impl crate::window::Window for AboutWindow {
+    fn window(&mut self, ctx: &egui::Context) {
+        egui::Window::new("About")
+            .open(&mut self.alive)
+            .auto_sized()
+            .show(ctx, |ui| {
+                ui.heading("Amanda's Samurai Planner");
+                ui.label(format!("Version {}", env!("CARGO_PKG_VERSION")));
+
+                ui.add_space(12.0);
+
+                ui.label("Built for FFXIV 7.38");
+
+                ui.add_space(12.0);
+
+                {
+                    ui.heading("Built with:");
+
+                    ui.hyperlink_to(
+                        "Rust",
+                        "https://rust-lang.org/",
+                    );
+
+                    ui.hyperlink_to(
+                        "egui and eframe",
+                        "https://github.com/emilk/egui",
+                    );
+                }
+            });
+    }
+
+    fn alive(&self) -> bool {
+        self.alive
+    }
+}
